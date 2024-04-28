c24ae2d9de8d535fe3751666124515d5e48a68e4
UjjawalRoy <royujjawal@gmail.com>
2024-04-28 09:06:36 +0530

    Updated file names

--- 01_num_grad.py
+++ 01_num_grad.py
@@ -6,7 +6,7 @@ weights = [[-1, 1.5, -0.3, 1.2],
            [1.3, 0.5, 2.3, -1.2],      
            [2.3, -0.05, 2.3, -0.2]]
 
-biases = biases = [3, 1, 0.5]
+biases = [3, 1, 0.5]
 
 output = np.dot(weights, inputs) + biases       # putting weights first since matrix dot product needs (x, y).(y, z) shaped matrices
 print(output)
